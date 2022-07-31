88ca95ce86b871364a0b5cafab3e034304825049
Namdeo999 <namdeo.madhi999@gmail.com>
2022-07-31 21:56:06 +0530

    update code

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
-import 'package:demo_app/example_four.dart';
-import 'package:demo_app/example_three.dart';
-import 'package:demo_app/example_two.dart';
-import 'package:demo_app/signup.dart';
+import 'package:demo_app/pages/home_page.dart';
+import 'package:demo_app/pages/login_page.dart';
+import 'package:demo_app/utils/routes.dart';
 import 'package:flutter/material.dart';
-import 'package:demo_app/home_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,12 +14,50 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      // home: HomePage(),
+      themeMode: ThemeMode.light,
       theme: ThemeData(
-        
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.lato().fontFamily,
+        // primaryTextTheme: GoogleFonts.latoTextTheme()
       ),
-      home: const ExampleFour(),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      initialRoute: '/login',
+      routes: {
+        '/': (context) => const LoginPage(),
+        MyRoutes.homeRoute: (context) => const HomePage(),
+        MyRoutes.loginRoute: (context) => const LoginPage(),
+      },
     );
   }
 }
+
+
+
+// import 'package:demo_app/example_fiver.dart';
+// import 'package:demo_app/example_four.dart';
+// import 'package:demo_app/example_three.dart';
+// import 'package:demo_app/example_two.dart';
+// import 'package:demo_app/signup.dart';
+// import 'package:flutter/material.dart';
+// import 'package:demo_app/home_screen.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+        
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: const ExampleThree(),
+//     );
+//   }
+// }
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,143 @@
+import 'package:demo_app/utils/routes.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name = '';
+  bool changeButton = false;
+  final _formKey = GlobalKey<FormState>();
+
+  moveToHome(BuildContext context) async {
+    if (_formKey.currentState!.validate()) {
+      setState(() {
+        changeButton = true;
+      });
+      await Future.delayed(Duration(seconds: 1));
+      await Navigator.pushNamed(context, MyRoutes.homeRoute);
+      setState(() {
+        changeButton = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // print("object");
+    return Material(
+      color: Colors.white,
+      child: SingleChildScrollView(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              Image.asset(
+                'assets/images/hey_img.png',
+                fit: BoxFit.cover,
+              ),
+              SizedBox(height: 20.0),
+              Text(
+                'Welcome $name',
+                style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 20.0),
+              Padding(
+                // padding: const EdgeInsets.all(16.0),
+                padding: const EdgeInsets.symmetric(
+                    vertical: 16.0, horizontal: 24.0),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(
+                        hintText: 'Enter username',
+                        labelText: 'Username',
+                      ),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Username cannot be empty";
+                        }
+                        return null;
+                      },
+                      onChanged: (value) {
+                        name = value;
+                        setState(() {});
+                      },
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        hintText: 'Enter password',
+                        labelText: 'Password',
+                      ),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Password cannot be empty";
+                        } else if (value!.length < 8) {
+                          return "Password length should be 8";
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(height: 40.0),
+
+                    Material(
+                      color: Colors.deepPurple,
+                      borderRadius:
+                          BorderRadius.circular(changeButton ? 50 : 8),
+                      child: InkWell(
+                        onTap: () => moveToHome(context),
+                        child: AnimatedContainer(
+                          duration: Duration(seconds: 1),
+                          width: changeButton ? 50 : 150,
+                          height: 50,
+                          alignment: Alignment.center,
+                          child: changeButton
+                              ? Icon(
+                                  Icons.done,
+                                  color: Colors.white,
+                                )
+                              : Text(
+                                  "Login",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 18),
+                                ),
+                          // decoration: BoxDecoration(
+                          //   color: Colors.deepPurple,
+                          //   borderRadius:
+                          //       BorderRadius.circular(changeButton ? 50 : 8),
+                          // ),
+                        ),
+                      ),
+                    ),
+
+                    // ElevatedButton(
+                    //   child: Text('Login'),
+                    //   style: TextButton.styleFrom(minimumSize: Size(150, 40)),
+                    //   onPressed: () {
+                    //     // print('Login button clicked');
+                    //     Navigator.pushNamed(context, MyRoutes.homeRoute);
+                    //   },
+                    // )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+      // child: Center(
+      //     child: Text(
+      //   'Login Page',
+      //   style: TextStyle(
+      //       fontSize: 30, color: Colors.blue, fontWeight: FontWeight.bold),
+      // )),
+    );
+  }
+}
